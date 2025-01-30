d42ce583503989700bb0d7d6c82c22146a28d90d
Edwar Jhoel Vargas <edwarjhoel.99@gmail.com>
2025-01-29 22:36:22 -0500

    agregar nueva columna con nombre accion a la tabla realizacion examen

--- src/pages/RealizacionExamen.jsx
+++ src/pages/RealizacionExamen.jsx
@@ -67,6 +67,7 @@ const RealizacionExamen = () => {
                                     <th className="px-4 py-2 border-b">Médico</th>
                                     <th className="px-4 py-2 border-b">Fecha</th>
                                     <th className="px-4 py-2 border-b">Estado</th>
+                                    <th className="px-4 py-2 border-b">Accion</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -81,6 +82,14 @@ const RealizacionExamen = () => {
                                             <span className={`px-2 py-1 text-white rounded ${examen.estado === "PENDIENTE" ? "bg-green-600" : "bg-red-600"}`}>
                                                 {examen.estado}
                                             </span>
+                                        </td>
+                                        <td className="border px-4 py-2">
+                                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mr-2">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                                    <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                                                    <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+                                                </svg>
+                                            </button>
                                         </td>
                                     </tr>
                                 ))}
